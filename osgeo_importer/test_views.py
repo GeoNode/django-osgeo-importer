a72af51d5118bbe7657ca83318f64fef03f5a852
Sasha Hart <harts@boundlessgeo.com>
2016-09-23 18:00:19 -0500

    set upload name and file type where feasible
    Since some of the multifile and geopackage changes, it is no longer obvious
    what should be the name of an upload. If it just contains one file, we can do
    as before, but otherwise things can become more difficult.
    
    The heuristics for this are a little complex, so they've been split off into
    their own method and tests created just for this method.

---
+++ osgeo_importer/test_views.py
@@ -0,0 +1,81 @@
+from django.test import TestCase
+from osgeo_importer import views
+from osgeo_importer import models
+
+
+class TestFileAddView_upload(TestCase):
+    """Test the helper method FileAddView.upload.
+    """
+
+    class FakeRequest(object):
+        """Fake the kind of request object used by FileAddView.upload.
+        """
+        def __init__(self, user):
+            self.user = user
+
+    class FakeFile(object):
+        """Fake the kind of file object used by FileAddView.upload.
+        """
+        def __init__(self, name):
+            self.name = name
+
+    def view(self):
+        view = views.FileAddView()
+        view.get_file_type = lambda path: "BogusType"
+        user = None
+        view.request = self.FakeRequest(user)
+        return view
+
+    def test_empty(self):
+        """Empty data should return None.
+
+        Form validation should guard against this sort of argument coming to
+        upload(), but if it does happen, upload() should behave reasonably.
+        """
+        data = []
+        view = self.view()
+        upload = view.upload(data)
+        self.assertEqual(upload.name, None)
+        self.assertEqual(upload.file_type, None)
+
+    def test_single(self):
+        data = [
+            self.FakeFile("/tmp/xyz/abc/foo.shp")
+        ]
+        view = self.view()
+        upload = view.upload(data)
+        self.assertEqual(upload.name, "foo.shp")
+        self.assertEqual(upload.file_type, "BogusType")
+
+    def test_double(self):
+        data = [
+            self.FakeFile("/tmp/xyz/abc/foo.shp"),
+            self.FakeFile("/tmp/xyz/abc/bar.shp")
+        ]
+        view = self.view()
+        upload = view.upload(data)
+        self.assertEqual(upload.name, "bar.shp, foo.shp")
+        self.assertEqual(upload.file_type, None)
+
+    def test_single_too_long(self):
+        max_length = models.UploadedData._meta.get_field('name').max_length
+        too_long = "ObviouslyWayWayWayWayWayWayWayWayWayWayWayWayWayWayTooLong"
+        self.assertGreater(too_long, max_length)
+        data = [
+            self.FakeFile("/tmp/{0}".format(too_long))
+        ]
+        view = self.view()
+        upload = view.upload(data)
+        self.assertEqual(upload.name.startswith("Obviously"), True)
+        self.assertEqual(upload.file_type, "BogusType")
+
+    def test_many_too_long(self):
+        data = [
+            self.FakeFile("/tmp/xyz/abc/NotTooLongOnItsOwn.shp"),
+            self.FakeFile("/tmp/xyz/abc/rather_long_to_be_combined_with.shp"),
+            self.FakeFile("/tmp/really_too_much.shp"),
+        ]
+        view = self.view()
+        upload = view.upload(data)
+        self.assertEqual(upload.name, None)
+        self.assertEqual(upload.file_type, None)
--- osgeo_importer/views.py
+++ osgeo_importer/views.py
@@ -2,9 +2,11 @@ import os
 import json
 import logging
 import shutil
+import collections
 from django.http import HttpResponse
 from django.views.generic import FormView, ListView, TemplateView
 from django.core.urlresolvers import reverse_lazy
+from django.utils.text import Truncator
 from .forms import UploadFileForm
 from .models import UploadedData, UploadLayer, UploadFile, DEFAULT_LAYER_CONFIGURATION
 from .importers import OSGEO_IMPORTER
@@ -79,8 +81,86 @@ class FileAddView(FormView, ImportHelper, JSONResponseMixin):
     template_name = 'osgeo_importer/new.html'
     json = False
 
-    def form_valid(self, form):
+    def upload(self, data):
+        """Use cleaned form data to populate an unsaved upload record.
+
+        Once, each upload was just one file, so all we had to do was attach its
+        name and type to the upload, where we could display it. Since multifile
+        and geopackage supported were added, the required behavior depends on
+        what we find in the upload. That is what this method is for. For
+        example, it looks at an upload and tries to come up with a reasonably
+        mnemonic name, or else set name to None to mean there was no obvious
+        option.
+        """
+        # Do not save here, we want to leave control of that to the caller.
         upload = UploadedData.objects.create(user=self.request.user)
+        # Get a list of paths for files in this upload.
+        paths = [item.name for item in data]
+        # If there are multiple paths, see if we can boil them down to a
+        # smaller number of representative paths (ideally, just one).
+        if len(paths) > 1:
+            # Group paths by common pre-extension prefixes
+            groups = collections.defaultdict(list)
+            for path in paths:
+                group_name = os.path.splitext(path)[0]
+                groups[group_name].append(path)
+            # Check each group for "leaders" - a special filename like "a.shp"
+            # which can be understood to represent other files in the group.
+            # Map from each group name to a list of leaders in that group.
+            leader_exts = ["shp"]
+            group_leaders = {}
+            for group_name, group in groups.items():
+                leaders = [
+                    path for path in group
+                    if any(path.endswith(ext) for ext in leader_exts)
+                ]
+                if leaders:
+                    group_leaders[group_name] = leaders
+            # Rebuild paths: leaders + paths without leaders to represent them
+            leader_paths = []
+            for leaders in group_leaders.values():
+                leader_paths.extend(leaders)
+            orphan_paths = []
+            for group_name, group in groups.items():
+                if group_name not in group_leaders:
+                    orphan_paths.extend(group)
+            paths = leader_paths + orphan_paths
+
+        max_length = UploadedData._meta.get_field('name').max_length
+        # If no data, just return the instance.
+        if not paths:
+            name = None
+            file_type = None
+        # If we just have one path, that's a reasonable name for the upload.
+        # We want this case to happen as frequently as reasonable, so that we
+        # can set one reasonable name and file_type for the whole upload.
+        elif len(paths) == 1:
+            path = paths[0]
+            basename = os.path.basename(path)
+            name = Truncator(basename).chars(max_length)
+            file_type = self.get_file_type(path)
+        # Failing that, see if we can represent all the important paths within
+        # the available space, making a meaningful mnemonic that isn't
+        # misleading even though there isn't one obvious name. But if we can't
+        # even do that, no use generating misleading or useless strings here,
+        # just pass a None and let the frontend handle the lack of information.
+        else:
+            basenames = sorted(os.path.basename(path) for path in paths)
+            name = ", ".join(basenames)
+            if len(name) > max_length:
+                logger.warning(
+                    "rejecting upload name for length: {0!r} {1} > {2}"
+                    .format(name, len(name), max_length)
+                )
+                name = None
+            file_type = None
+
+        upload.name = name
+        upload.file_type = file_type
+        return upload
+
+    def form_valid(self, form):
+        upload = self.upload(form.cleaned_data['file'])
         upload.save()
 
         # Create Upload Directory based on Upload PK
