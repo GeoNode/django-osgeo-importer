bb40736e27efc8a884f894e3520c811c06906b92
Tyler Garner <garnertb@gmail.com>
2016-10-16 14:35:42 -0400

    Travis fix.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
+set -e
+
 sudo apt-get -qq -y update
 # geoserver
 sudo apt-get install -y --force-yes openjdk-7-jdk --no-install-recommends
 sudo apt-get install -y unzip
-mkdir gs
+mkdir -p gs
 pushd gs
 wget http://repo2.maven.org/maven2/org/mortbay/jetty/jetty-runner/8.1.8.v20121106/jetty-runner-8.1.8.v20121106.jar
 wget http://ares.boundlessgeo.com/geoserver/${GS_VERSION}/geoserver-${GS_VERSION}-latest-war.zip
 chmod +x jetty-runner-8.1.8.v20121106.jar
 unzip -o geoserver-${GS_VERSION}-latest-war.zip -d .
 popd
-# postgis
-sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable -y # For postgresql-9.1-postgis-2.1
-sudo rm -f /etc/apt/sources.list.d/pgdg-source.list # postgis from pgdg requires different gdal package than the grass package
 sudo apt-get update -qq
 
 wget https://s3.amazonaws.com/django-osgeo-importer/gdal-2.2.0-linux-bin.tar.gz
@@ -26,14 +25,23 @@ sudo ldconfig
 sudo touch /etc/profile.d/gdal
 sudo su -c "echo 'export GDAL_DATA=/usr/local/lib/gdal/share/gdal/' >> /etc/profile.d/gdal.sh"
 
+if ["$TRAVIS" = true];
+then
+   sudo apt-get -y --no-install-recommends --force-yes install postgresql-9.3-postgis-2.2
+else
+   sudo add-apt-repository "deb https://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main"
+   wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+   sudo apt-get update
+   sudo apt-get -y install postgresql-9.3-postgis-2.2
+fi
 
-sudo apt-get remove -y postgresql-9.3-postgis-2.1 # Remove postgis from pgdg, will install postgis from ubuntugis-unstable instead
-sudo apt-get install -y --no-install-recommends postgresql-9.3-postgis-2.1 libpq-dev python-dev python-lxml libxslt1-dev
-sudo apt-get install -y python-virtualenv python-imaging python-pyproj python-shapely python-nose python-httplib2 python-httplib2 gettext git
-sudo apt-get install -y libproj0 libproj-dev postgresql-plpython-9.3 python-numpy python-dateutil libjpeg62 libjpeg-dev zlib1g-dev python-dev libxml2 libxml2-dev libxslt1-dev libpq-dev postgis
+sudo apt-get install -y python-dev python-lxml libxslt1-dev  libpq-dev
+sudo apt-get install -y python-virtualenv python-imaging python-pyproj libgeos-3.4.2 libgeos-dev python-shapely python-nose python-httplib2 python-httplib2 gettext git
+sudo apt-get install -y libproj0 libproj-dev postgresql-plpython-9.3 python-numpy python-dateutil libjpeg62 zlib1g-dev python-dev libxml2 libxml2-dev libxslt1-dev libpq-dev
+
 #install libraries for a stacked out gdal
-sudo apt-get install -y sqlite3 libsqlite3-0 libsqlite3-dev libspatialite5 libspatialite-dev libcurl4-gnutls-dev libxerces-c-dev libxml2-dev 
-sudo apt-get install -y gpsbabel libfreexl-dev unixodbc-dev libwebp-dev libjpeg-dev libpq-dev libpng12-dev libjpeg-dev libgif-dev liblzma-dev
+sudo apt-get install -y sqlite3 libsqlite3-0 libsqlite3-dev libspatialite5 libspatialite-dev libcurl4-gnutls-dev libxerces-c-dev
+sudo apt-get install -y gpsbabel libfreexl-dev unixodbc-dev libwebp-dev libjpeg-dev libpng12-dev libgif-dev liblzma-dev
 sudo apt-get install -y libcrypto++-dev netcdf-bin libnetcdf-dev libexpat-dev
 
 
