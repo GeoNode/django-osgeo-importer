cfec01b4e7ef5cddf914f09633fb769c51f87593
Jivan Amara <Development@JivanAmara.net>
2016-11-10 11:26:48 -0800

    Moved setting _TEST_FILES_DIR from tests_original -> test_settings.

---
+++ osgeo_importer/tests/test_settings.py
@@ -0,0 +1,10 @@
+'''
+Created on Nov 9, 2016
+
+@author: jivan
+'''
+import os
+
+# Set the location of test files in one place instead of repeating
+_TEST_FILES_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'importer-test-files'))
