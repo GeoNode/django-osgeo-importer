844df83808d9063b4d26c53ed0767173997692e1
Jivan Amara <Development@JivanAmara.net>
2016-12-13 16:14:35 -0800

    Initial GPKGTileInspector with basic test.

--- osgeo_importer/tests/test_inspectors.py
+++ osgeo_importer/tests/test_inspectors.py
@@ -1,5 +1,10 @@
+import os
+
 from django.test import SimpleTestCase
-from osgeo_importer.inspectors import OGRInspector, GDALInspector
+
+from osgeo_importer.inspectors import OGRInspector, GDALInspector, GPKGTileInspector
+from osgeo_importer.tests.helpers import FuzzyFloatCompareDict
+from osgeo_importer.tests.test_settings import _TEST_FILES_DIR
 from osgeo_importer.utils import NoDataSourceFound
 
 
@@ -23,3 +28,37 @@ class TestOGRInspector(SimpleTestCase):
 class TestGDALInspector(SimpleTestCase):
     def test_open_bad_connection(self):
         check_inspector_open_bad_connection(self, GDALInspector)
+
+
+class TestGPKGTileInspector(SimpleTestCase):
+    def test_describe_fields(self):
+        # [ <basic gpkg with tiles> ]
+        filenames = ['sde-NE2_HR_LC_SR_W_DR.gpkg']
+        # Each element is a list of dicts for one of the above files one, one dict for each tile layer found.
+        expected_describe_field_results = {
+            'sde-NE2_HR_LC_SR_W_DR.gpkg': [
+                    {
+                        'index': 0, 'srs_id': 4326, 'layer_name': u'NE2_HR_LC_SR_W_DR',
+                        'latlong_bbox': [-180.0, -90.0, 180.0, 90.0]
+                    },
+                ]
+            }
+
+        for filename in filenames:
+            filepath = os.path.join(_TEST_FILES_DIR, filename)
+            expected_result = expected_describe_field_results[filename]
+            with GPKGTileInspector(filepath) as insp:
+                def float_eq(a, b):
+                    return abs(a - b) < 0.001
+
+                detail_list = [
+                    FuzzyFloatCompareDict(d, float_eq=float_eq) for d in insp.describe_fields()
+                ]
+                expected_detail_list = [
+                    FuzzyFloatCompareDict(d, float_eq=float_eq) for d in expected_result
+                ]
+
+                self.assertEqual(len(detail_list), len(expected_detail_list))
+                for d, ed in zip(detail_list, expected_detail_list):
+                    msg = '\n{}\n !=\n{}'.format(d, ed)
+                    self.assertEqual(d, ed, msg)
