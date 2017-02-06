3bbcfc9731e171bba674917fabd8a70832bf25aa
Daniel Berry <dberry@boundlessgeo.com>
2017-02-05 20:39:48 -0600

    remove hard dependencies via pip install (#108)
    reconfigured setup and manifest to package all required files

--- setup.py
+++ setup.py
@@ -8,49 +8,33 @@ with open('README.md') as f:
 setup(
     name='django-osgeo-importer',
     version='.'.join(str(i) for i in osgeo_importer.__version__),
-    description='django-osgeo-importer is a reusable Django application for inspecting geospatial data using GDAL/OGR and importing the data into an application.',
+    description='django-osgeo-importer is a reusable Django application for '
+                'inspecting geospatial data using GDAL/OGR and importing the '
+                'data into an application.',
     long_description=readme,
     author='Tyler Garner',
     author_email='garnertb@prominentedge.com',
     maintainer='django-osgeo-importer contributors',
     url='https://github.com/GeoNode/django-osgeo-importer',
-    # osgeo_importer_prj is only used for development & testing
-    packages=('osgeo_importer',),
-    package_data={
-        'osgeo_importer': [
-            'locale/*/LC_MESSAGES/*',
-            'templates/osgeo_importer/*',
-            # Include all of the static files except for the 'test' subdir.
-            'static/osgeo_importer/css/*',
-            'static/osgeo_importer/factories.js',
-            'static/osgeo_importer/importer.js',
-            'static/osgeo_importer/karma.conf.js',
-            'static/osgeo_importer/package.json',
-            'static/osgeo_importer/privacy.png',
-            'static/osgeo_importer/time.png',
-            'static/osgeo_importer/edit.png',
-            'static/osgeo_importer/img/*',
-            'static/osgeo_importer/js/*',
-            'static/osgeo_importer/Makefile',
-            'static/osgeo_importer/partials/*',
-        ],
-    },
+    # see MANIFEST for packaging exclusions
+    packages=find_packages('.'),
     license='GPLv3+',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: GNU General Public License v3 or later'
+        ' (GPLv3+)',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 2.7',
         'Framework :: Django :: 1.8',
     ],
     install_requires=[
-        'django-tastypie==0.12.2',
-        'python-dateutil==2.5.3',
-        'numpy==1.11.2',
-        'geonode==2.5.9',
-        'Django==1.8',
+        'django-tastypie>=0.12.2,<0.14',
+        'python-dateutil>=2.5.3,<2.7',
+        'numpy>=1.11.2,<1.12',
+        'geonode>=2.5.9,<2.6',
+        'Django>=1.8,<1.9',
     ],
     include_package_data=True,
     zip_safe=False,
