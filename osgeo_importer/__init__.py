8e6391124339b378a6b4dc542aaee14a197852c4
Jivan Amara <Development@JivanAmara.net>
2017-02-14 15:25:40 -0800

    Bumped version to 0.1.1a3.  Tagged as v0.1.1a3. (#124)

--- osgeo_importer/__init__.py
+++ osgeo_importer/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = (0, 1, 1, 'alpha', 2)
+__version__ = (0, 1, 1, 'alpha', 3)
 
 os.environ.setdefault('PGCLIENTENCODING', 'UTF-8')
 os.environ.setdefault('SHAPE_ENCODING', 'UTF-8')
