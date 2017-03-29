1beb53e11a19f2c99db1da26444553fe004f29c0
milafrerichs <mila.frerichs@gmail.com>
2017-03-29 18:11:23 +0200

    Add importer client to installed_apps

--- osgeo_importer_prj/settings.py
+++ osgeo_importer_prj/settings.py
@@ -77,7 +77,7 @@ LOCALE_PATHS = (
     ) + LOCALE_PATHS
 
 
-INSTALLED_APPS = INSTALLED_APPS + ("osgeo_importer",)
+INSTALLED_APPS = INSTALLED_APPS + ("osgeo_importer","osgeo_importer_client",)
 # # Remove 'geonode.geoserver', useful for experimenting with a geoserver-less configuration.
 # INSTALLED_APPS = [ a for a in INSTALLED_APPS if a != 'geonode.geoserver' ]
 
