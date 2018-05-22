2bdd45bb42d67fe51ed6e516c2d24fda8adb5f02
Hisham waleed karam <hisham.karam@cartologic.com>
2018-05-22 22:27:55 +0200

    geonode2.8.x compatibility (#211)
    * fix gdal imports on macos
    
    * fix templates with geonode2.8.x
    
    * add OVERWRITE=YES option with ogr
    
    * fix requirements.dev.txt
    
    * add geonode2.8rc11 to requirements.dev.txt

--- osgeo_importer/importers.py
+++ osgeo_importer/importers.py
@@ -4,9 +4,12 @@ import os
 from django import db
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
-import gdal
-import ogr
-import osr
+try:
+    import gdal
+    import ogr
+    import osr
+except:
+    from osgeo import gdal, ogr, osr
 
 from osgeo_importer.models import UploadLayer
 
@@ -32,10 +35,13 @@ MEDIA_ROOT = getattr(settings, 'MEDIA_ROOT', FileSystemStorage().location)
 DEFAULT_SUPPORTED_EXTENSIONS = ['shp', 'shx', 'prj', 'dbf', 'kml', 'geojson', 'json',
                                 'tif', 'tiff', 'gpkg', 'csv', 'zip', 'xml',
                                 'sld', 'ntf', 'nitf']
-VALID_EXTENSIONS = getattr(settings, 'OSGEO_IMPORTER_VALID_EXTENSIONS', DEFAULT_SUPPORTED_EXTENSIONS)
+VALID_EXTENSIONS = getattr(
+    settings, 'OSGEO_IMPORTER_VALID_EXTENSIONS', DEFAULT_SUPPORTED_EXTENSIONS)
 
-RASTER_FILES = getattr(settings, 'OSGEO_IMPORTER_RASTER_FILES', os.path.join(MEDIA_ROOT, 'osgeo_importer_raster'))
-UPLOAD_DIR = getattr(settings, 'OSGEO_IMPORTER_UPLOAD_DIR', os.path.join(MEDIA_ROOT, 'osgeo_importer_uploads'))
+RASTER_FILES = getattr(settings, 'OSGEO_IMPORTER_RASTER_FILES', os.path.join(
+    MEDIA_ROOT, 'osgeo_importer_raster'))
+UPLOAD_DIR = getattr(settings, 'OSGEO_IMPORTER_UPLOAD_DIR',
+                     os.path.join(MEDIA_ROOT, 'osgeo_importer_uploads'))
 
 if not os.path.exists(RASTER_FILES):
     os.makedirs(RASTER_FILES)
@@ -115,7 +121,8 @@ class Import(object):
         layers = self.import_file(configuration_options=configuration_options)
 
         for layer, config in layers:
-            config['handler_results'] = self.run_import_handlers(layer, config, **kwargs)
+            config['handler_results'] = self.run_import_handlers(
+                layer, config, **kwargs)
 
         return layers
 
@@ -130,7 +137,8 @@ class Import(object):
         """
         self.handler_results = []
         for handler in self.import_handlers:
-            self.handler_results.append({type(handler).__name__: handler.handle(layer, layer_config, *args, **kwargs)})
+            self.handler_results.append(
+                {type(handler).__name__: handler.handle(layer, layer_config, *args, **kwargs)})
 
         return self.handler_results
 
@@ -174,10 +182,10 @@ class OGRImport(Import):
         if target_store is None:
             d = db.connections[settings.OSGEO_DATASTORE].settings_dict
             connection_string = "PG:dbname='%s' user='%s' password='%s' host='%s' port='%s' schemas=%s" % (
-                                                                                                d['NAME'], d['USER'],
-                                                                                                d['PASSWORD'],
-                                                                                                d['HOST'], d['PORT'],
-                                                                                                database_schema_name())
+                d['NAME'], d['USER'],
+                d['PASSWORD'],
+                d['HOST'], d['PORT'],
+                database_schema_name())
             self.target_store = connection_string
 
     def open_target_datastore(self, connection_string, *args, **kwargs):
@@ -199,9 +207,11 @@ class OGRImport(Import):
         """
         geom_types = []
         if type == 'name':
-            [geom_types.append(f.geometry().GetGeometryName()) for f in layer if f.geometry()]
+            [geom_types.append(f.geometry().GetGeometryName())
+             for f in layer if f.geometry()]
         else:
-            [geom_types.append(f.geometry().GetGeometryType()) for f in layer if f.geometry()]
+            [geom_types.append(f.geometry().GetGeometryType())
+             for f in layer if f.geometry()]
 
         geom_types.append(layer.GetGeomType())
         geom_types = list(set(geom_types))
@@ -236,13 +246,14 @@ class OGRImport(Import):
                 if all(t in features_geom_types for t in types_dict[k]):
                     plural, singular = k.split('_')
                     logger.warn("Found {plural} and {singular} geometry types in dataset, using {plural}".format(
-                                      plural=plural, singular=singular))
+                        plural=plural, singular=singular))
                     layer_geom_type = max(types_dict[k])
                     break
 
             # Cover a case where KML/LIBKML layer geometry type is Geometry/Unknown, but don't fail
             if layer_geom_type == 0:
-                layer_geom_type = max(features_geom_types) if features_geom_types else layer_geom_type
+                layer_geom_type = max(
+                    features_geom_types) if features_geom_types else layer_geom_type
 
         return layer_geom_type
 
@@ -260,7 +271,8 @@ class OGRImport(Import):
         err = GdalErrorHandler()
         gdal.PushErrorHandler(err.handler)
         gdal.UseExceptions()
-        configuration_options = kwargs.get('configuration_options', [{'index': 0}])
+        configuration_options = kwargs.get(
+            'configuration_options', [{'index': 0}])
         # Configuration options should be a list at this point since the
         # importer can process multiple layers in a single import
         if isinstance(configuration_options, dict):
@@ -273,7 +285,8 @@ class OGRImport(Import):
                 nbad_config += 1
 
         if nbad_config > 0:
-            msg = '{} of {} configs missing upload_layer_id'.format(nbad_config, len(configuration_options))
+            msg = '{} of {} configs missing upload_layer_id'.format(
+                nbad_config, len(configuration_options))
             logger.critical(msg)
             raise Exception(msg)
 
@@ -282,7 +295,8 @@ class OGRImport(Import):
         # If layer_name is present in configuration_options either update UploadLayer.layer_name to match if it's unique
         #    or update configuration_options' 'layer_name' to match value in UploadLayer.layer_name if it's not unique.
         with db.transaction.atomic():
-            upload_layer_ids = [co['upload_layer_id'] for co in configuration_options]
+            upload_layer_ids = [co['upload_layer_id']
+                                for co in configuration_options]
             upload_layers = UploadLayer.objects.filter(id__in=upload_layer_ids)
             upload_layers_by_id = {ul.id: ul for ul in upload_layers}
 
@@ -333,12 +347,14 @@ class OGRImport(Import):
                         # This update will overwrite the layer_name passed in configuration_options, stash the
                         #    intended name so we can correct it.
                         msg = 'Will configure layer from file {} identifed by field "{}" with value {}'\
-                                  .format(self.file, lf, layer_configuration[lf])
+                            .format(self.file, lf, layer_configuration[lf])
                         logger.info(msg)
-                        intended_layer_name = layer_configuration.get('layer_name')
+                        intended_layer_name = layer_configuration.get(
+                            'layer_name')
                         layer_configuration.update(datastore_layer)
                         if intended_layer_name:
-                            layer_configuration.update({'layer_name': intended_layer_name})
+                            layer_configuration.update(
+                                {'layer_name': intended_layer_name})
                         else:
                             msg = ('layer_name not provided in configuration options, will use name provided '
                                    'by inspector which will likely lead to name collisions')
@@ -351,7 +367,8 @@ class OGRImport(Import):
                 # No special processing is needed on import, the only thing needed is a copy of the
                 #    file which was made on upload.  Config for publishing is done
                 #    in handlers.mapproxy.publish_handler.MapProxyGPKGTilePublishHandler
-                self.completed_layers.append([layer_options['layer_name'], layer_options])
+                self.completed_layers.append(
+                    [layer_options['layer_name'], layer_options])
             elif layer_options['layer_type'] == 'raster':
                 """
                 File is a raster, we need to convert into optimized GeoTiff
@@ -359,8 +376,10 @@ class OGRImport(Import):
                 """
                 #  Increment filename to make sure target doesn't exists
                 filedir, filebase = os.path.split(filename)
-                outfile = "{}/{}.tif".format(filedir, layer_options['layer_name'].lower())
-                fileout = increment_filename(os.path.join(RASTER_FILES, outfile))
+                outfile = "{}/{}.tif".format(filedir,
+                                             layer_options['layer_name'].lower())
+                fileout = increment_filename(
+                    os.path.join(RASTER_FILES, outfile))
                 raster_import(layer_options['path'], fileout)
                 self.completed_layers.append([fileout, layer_options])
             elif layer_options['layer_type'] == 'vector':
@@ -370,6 +389,7 @@ class OGRImport(Import):
                 # Prevent numeric field overflow for shapefiles https://trac.osgeo.org/gdal/ticket/5241
                 if target_file.GetDriver().GetName() == 'PostgreSQL':
                     target_create_options.append('PRECISION=NO')
+                    target_create_options.append('OVERWRITE=YES')
                     # Hack for CSV ingest into postgres. When using COPY, OGR prepends a bad newline to each feature
                     if data.GetDriver().ShortName.lower() == 'csv':
                         os.environ["PG_USE_COPY"] = "false"
@@ -389,7 +409,8 @@ class OGRImport(Import):
 
                 # pass the srs authority code to handlers
                 if srs.AutoIdentifyEPSG() == 0:
-                    layer_options['srs'] = '{0}:{1}'.format(srs.GetAuthorityName(None), srs.GetAuthorityCode(None))
+                    layer_options['srs'] = '{0}:{1}'.format(
+                        srs.GetAuthorityName(None), srs.GetAuthorityCode(None))
                 else:
                     # layer_options['srs'] = convert_wkt_to_epsg(srs.ExportToWkt())
                     layer_ids = []
@@ -398,13 +419,17 @@ class OGRImport(Import):
                     layer_id = layer_ids[0]
                     layer_path = os.path.dirname(filename)
                     original_layer_name = layer.GetName()
-                    layer_options['srs'] = reproject_coordinate_system(original_layer_name, layer_name, layer, layer_path)
-                    data, inspector = self.open_source_datastore(filename, *args, **kwargs)
-                    target_file, _ = self.open_target_datastore(self.target_store)
+                    layer_options['srs'] = reproject_coordinate_system(
+                        original_layer_name, layer_name, layer, layer_path)
+                    data, inspector = self.open_source_datastore(
+                        filename, *args, **kwargs)
+                    target_file, _ = self.open_target_datastore(
+                        self.target_store)
                     layer = data.GetLayer(layer_options.get('index'))
                     srs = layer.GetSpatialRef()
 
-                logger.info('Creating dataset "{}" from file "{}"'.format(layer_name, target_file))
+                logger.info('Creating dataset "{}" from file "{}"'.format(
+                    layer_name, target_file))
                 target_layer = self.create_target_dataset(target_file, str(layer_name), srs, layer_geom_type,
                                                           options=target_create_options)
 
@@ -453,7 +478,8 @@ class OGRImport(Import):
                             else:
                                 conversion_function = ogr.ForceToMultiPolygon
 
-                            geom = ogr.CreateGeometryFromWkb(feature.geometry().ExportToWkb())
+                            geom = ogr.CreateGeometryFromWkb(
+                                feature.geometry().ExportToWkb())
                             feature.SetGeometry(conversion_function(geom))
 
                         if source_fid is not None:
@@ -464,18 +490,22 @@ class OGRImport(Import):
                                 try:
                                     feature.GetField(field).decode('utf8')
                                 except UnicodeDecodeError:
-                                    feature.SetField(field, decode(feature.GetField(field)))
+                                    feature.SetField(field, decode(
+                                        feature.GetField(field)))
                                 except AttributeError:
                                     continue
                         try:
                             target_layer.CreateFeature(feature)
                         except err as e:
-                            logger.error('Create feature failed: {0}'.format(gdal.GetLastErrorMsg()))
+                            logger.error('Create feature failed: {0}'.format(
+                                gdal.GetLastErrorMsg()))
                             raise e
                 layer.ResetReading()
-                self.completed_layers.append([target_layer.GetName(), layer_options])
+                self.completed_layers.append(
+                    [target_layer.GetName(), layer_options])
             else:
-                msg = 'Unexpected layer type: "{}"'.format(layer_options['layer_type'])
+                msg = 'Unexpected layer type: "{}"'.format(
+                    layer_options['layer_type'])
                 logger.error(msg)
                 raise Exception(msg)
 
